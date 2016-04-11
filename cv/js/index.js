6249efee71aebaad92c96846a50715982af0a826
tmejovsek <thomasmejovsek@hotmail.com>
2016-04-11 10:59:18 +0200

    meilleur version du cv

--- cv/js/index.js
+++ cv/js/index.js
@@ -1,18 +1,21 @@
 $( document ).ready(function() {
 
-
   $('#btnOpen').on('click', function() {
     if ( $('#contenu').hasClass('openContenu') ) {
       $('#contenu').css('width', '0%');
+      $('#contentPres').css('width', '50%');
       $('#accueil').css('transform', 'skewX(-25deg)');
+      $('#accueil').css('left', '-50%');
       $('#contenu').removeClass("openContenu");
       $('#content').css('opacity', '0');
       $('#cross').addClass('hide-cross');
     }
     else {
       // $('#contenu').toggle(true);
-      $('#contenu').css('width', '50%');
+      $('#contenu').css('width', '64%');
       $('#accueil').css('transform', 'skewX(0deg)');
+      $('#accueil').css('left', '-64%');
+      $('#contentPres').css('width', '36%');
       $('#contenu').addClass("openContenu");
       $('#content').css('opacity', '1');
       $('#cross').removeClass('hide-cross');
@@ -21,7 +24,9 @@ $( document ).ready(function() {
 
   $('#cross').on('click', function() {
     $('#contenu').css('width', '0%');
+    $('#contentPres').css('width', '50%');
     $('#accueil').css('transform', 'skewX(-25deg)');
+    $('#accueil').css('left', '-50%');
     $('#contenu').removeClass("openContenu");
     $('#content').css('opacity', '0');
     $('#cross').addClass('hide-cross');
